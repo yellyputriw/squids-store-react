34ceae56f4ea9350d4664fc76a0b6c736ee83824
Yelly Putri <yellyputriw@gmail.com>
2022-06-03 14:33:21 +0700

    :art: improve structure code of route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from '../components/Layout/Homepage';
+import Store from '../components/Layout/Store';
+import Profile from '../components/Layout/Profile';
+import ShoppingCart from '../components/Layout/ShoppingCart';
+import Blog from '../components/Layout/Blog';
 
 const App = () => {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Homepage />} />
+        <Route path='/store' exact element={<Store />} />
+        <Route path='/blog' exact element={<Blog />} />
+        <Route path='/profile' exact element={<Profile />} />
+        <Route path='/shoppingcart' exact element={<ShoppingCart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
